ea4f2df7e9e2a33fe4f16972ed8347c39e33b7dd
Kyle Brittan <kabrittan@gmail.com>
2018-06-24 17:35:52 -0700

    Added Images

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import land1 from './images/land1.jpg';
+import land2 from './images/land2.jpg';
+import land3 from './images/land3.jpg';
+import land4 from './images/land4.jpg';
+import land5 from './images/land5.jpg';
+import land6 from './images/land6.jpg';
+import land7 from './images/land7.jpg';
+import land8 from './images/land8.jpg';
+import land9 from './images/land9.jpg';
+import land10 from './imagesland10.jpg';
+import land11 from './images/land11.jpg';
+import land12 from './images/land12.jpg';
+
+const images = [
+  land1,
+  land2,
+  land3,
+  land4,
+  land5,
+  land6,
+  land7,
+  land8,
+  land9,
+  land10,
+  land11,
+  land12
+];
+
+export default images;
